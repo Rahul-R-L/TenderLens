5afcbb38f07e3358af6390145fef413af1d43152
Rahul <rahulrl191@gmail.com>
2026-06-27 23:41:13 +0530

    supabase

--- test_auth.py
+++
@@ -1,8 +0,0 @@
-from database.db_postgres import (
-    email_exists,
-    get_user_by_email
-)
-
-print(email_exists("tenderlens191@gmail.com"))
-
-print(get_user_by_email("tenderlens191@gmail.com"))
